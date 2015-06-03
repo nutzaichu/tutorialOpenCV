bea94ad66d1b95fc14bc9d9c74d04014dc8bd405
nutzaichu <nutzaichu@gmail.com>
2015-06-03 14:32:16 +0700

    cam

--- src/tutorialOpenCVApp.cpp
+++ src/tutorialOpenCVApp.cpp
@@ -2,7 +2,13 @@
 #include "cinder/gl/gl.h"
 #include "cinder/ImageIo.h"
 #include "cinder/gl/Texture.h"
-
+#include "cinder/app/AppBasic.h"
+#include "cinder/ImageIO.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Channel.h"
+#include "cinder/Cinder.h"
+#include "cinder/Capture.h"
+#include "cinder/gl/gl.h"
 #include "CinderOpenCv.h"
 
 using namespace ci;
@@ -11,31 +17,40 @@ using namespace ci::app;
 class tutorialOpenCVApp : public AppNative {
   public:
 	void setup();
+	void update();
 	void draw();
 	
-	gl::Texture	mTexture;
+	CaptureRef		mCapture;
+	gl::TextureRef	mTexture;
 };
 
 void tutorialOpenCVApp::setup()
 {
+
 	// The included image is copyright Trey Ratcliff
 	// http://www.flickr.com/photos/stuckincustoms/4045813826/
-	
-	ci::Surface8u surface( loadImage( loadAsset( "dfw.jpg" ) ) );
-	cv::Mat input( toOcv( surface ) );
-	cv::Mat output;
-
-	cv::medianBlur( input, output, 11 );
-//	cv::Sobel( input, output, CV_8U, 0, 1 ); 
-//	cv::threshold( input, output, 128, 255, CV_8U );
-
-	mTexture = gl::Texture( fromOcv( output ) );
+	mCapture = Capture::create(640, 480);
+	mCapture->start();
+	//ci::Surface8u surface( loadImage( loadAsset( "dfw.jpg" ) ) );
+	//cv::Mat input( toOcv( surface ) );
+	//cv::Mat output;
+	mTexture = gl::Texture::create(mCapture->getSurface());
+	//mTexture = gl::Texture( fromOcv( output ) );
 }   
-
+void tutorialOpenCVApp::update()
+{
+//	if (mCapture && mCapture->checkNewFrame()) {
+	mTexture = gl::Texture::create(mCapture->getSurface());
+//	}
+}
 void tutorialOpenCVApp::draw()
 {
 	gl::clear();
 	gl::draw( mTexture );
+}
+
+void bgSubstract(){
+
 }
 
 CINDER_APP_NATIVE( tutorialOpenCVApp, RendererGl )
