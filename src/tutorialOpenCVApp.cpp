db28f9470d4030816b420adf37183b54ce93ff50
nutzaichu <nutzaichu@gmail.com>
2015-06-03 17:11:29 +0700

    invis cloak

--- src/tutorialOpenCVApp.cpp
+++ src/tutorialOpenCVApp.cpp
@@ -21,23 +21,29 @@ class tutorialOpenCVApp : public AppNative {
 	void setup();
 	void update();
 	void draw();
-	
+	void keyDown(KeyEvent event);
 	Capture		mCapture;
 	Texture		mTexture;
 	Surface		mSurface;
+	Mat			img;
+	bool		first = true;
 };
 
 void tutorialOpenCVApp::setup()
 {
 	mCapture = Capture(640, 480);
 	mCapture.start();
+	Mat img(toOcv(Surface(mCapture.getSurface())));
+	
 }   
 void tutorialOpenCVApp::update()
 {
+	if (first) {
+		img = Mat(toOcv(Surface(mCapture.getSurface()))); first = false;
+	}
 	mSurface = Surface(mCapture.getSurface());
 	Mat input(toOcv(mSurface));
-	
-	Mat img(toOcv(Surface(loadImage(loadAsset("dfw.jpg")))));
+	//Mat img(toOcv(Surface(loadImage(loadAsset("dfw.jpg")))));
 
 	int R, G, B;
 	for (int i = 0; i < input.rows; i++){
@@ -45,20 +51,25 @@ void tutorialOpenCVApp::update()
 			R = input.at<cv::Vec3b>(i, j)[0];
 			G = input.at<cv::Vec3b>(i, j)[1];
 			B = input.at<cv::Vec3b>(i, j)[2];
-			if (G - R > 10 && G - B > 10){
+			if (G - R > 5 && G - B > 5){
 				input.at<cv::Vec3b>(i, j)[0] = img.at<cv::Vec3b>(i, j)[0];
 				input.at<cv::Vec3b>(i, j)[1] = img.at<cv::Vec3b>(i, j)[1];
 				input.at<cv::Vec3b>(i, j)[2] = img.at<cv::Vec3b>(i, j)[2];
 			}
 		}
 	}
-
 	mTexture = Texture(fromOcv(input));
-
 }
 void tutorialOpenCVApp::draw()
 {
 	gl::clear();
 	gl::draw( mTexture );
 }
+
+void tutorialOpenCVApp::keyDown(KeyEvent event){
+	if (event.getChar() == '1'){
+		img = Mat(toOcv(Surface(mCapture.getSurface())));
+	}
+}
+
 CINDER_APP_NATIVE( tutorialOpenCVApp, RendererGl )
